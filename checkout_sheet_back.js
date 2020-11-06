e15134beb7474c28c1d76e2fc2774bd05e86453c
Muriel Proton <muriel.proton@epitech.eu>
2020-11-06 16:15:18 +0100

    The form of the oppening cash flow is in a TABLE Function Summ is done

--- checkout_sheet_back.js
+++ checkout_sheet_back.js
@@ -1,6 +1,6 @@
-function date_yyyy_mm_dd(){
+function date_yyyy_mm_dd() {
     var fullDate = new Date();
-    var date = fullDate.getFullYear()+"_"+(fullDate.getMonth()+1)+"_"+fullDate.getDate();
+    var date = fullDate.getFullYear() + "_" + (fullDate.getMonth() + 1) + "_" + fullDate.getDate();
     return date;
 }
 function convertToPDF() {
@@ -13,12 +13,30 @@ function convertToPDF() {
     doc.text(160, 10, "Fermeture :");
     doc.text(195, 10, document.getElementById("fermeturePrenom").value);
     doc.text(235, 10, document.getElementById("fermetureNom").value);
-    doc.save('feuille_de_caisse_'+ date + '.pdf');
+    doc.save('feuille_de_caisse_' + date + '.pdf');
 }
 function euroBillTimesQuantity(input) {
-    console.log(input.getAttribute("targetDisplay"))
-    console.log(input.getAttribute("euroBill"))
     var euroBill = input.getAttribute("euroBill");
     var displayId = input.getAttribute("targetDisplay");
-    document.getElementById(displayId).innerHTML = (euroBill *input.value).toFixed(2);
+    document.getElementById(displayId).innerHTML = (euroBill * input.value).toFixed(2);
+    openingSumm();
+}
+
+function openingSumm() {
+    console.log("somme");
+    console.log(document.getElementById("Opening50xquantite").value);
+    var montant = document.getElementsByClassName("montant");
+    var openingSum = 0;
+    for (var i = 0; i < montant.length; i++) {
+        var contenu = montant.item(i).innerHTML;
+        console.log("item " + i);
+        console.log(contenu);
+        var contenuAsFloat = parseFloat(contenu);
+        if (!isNaN(contenuAsFloat)) {
+            console.log(contenuAsFloat);
+            openingSum = openingSum + contenuAsFloat;
+        }
+        console.log(openingSum);
+    }
+    document.getElementById("openingSumm").innerHTML =(openingSum).toFixed(2);
 }
